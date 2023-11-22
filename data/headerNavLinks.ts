62a0ef3db60e077e1098e1925aec4e0e1df1af7a
Apurva Shah <shahh.apurva@gmail.com>
2023-11-21 19:57:37 -0800

    fix(nav): fixed broken links on navbar

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,8 +2,8 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/browse', title: 'Tags' },
-  { href: 'https://apurvashah.org/projects', title: 'Projects' },
-  { href: 'https://apurvashah.org/about', title: 'About' },
+  { href: 'apurvashah.org/projects', title: 'Projects' },
+  { href: 'apurvashah.org/about', title: 'About' },
 ]
 
 export default headerNavLinks
