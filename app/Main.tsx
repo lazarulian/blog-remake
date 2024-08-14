ca8b1ad9ce682f1975ff2724144b542342778960
Apurva Shah <shahh.apurva@gmail.com>
2024-08-14 11:15:01 -0700

    feat(blogs): bruin bucket list

--- app/Main.tsx
+++ app/Main.tsx
@@ -16,7 +16,7 @@ export default function Home({ posts }) {
         <div className="my-6 flex flex-col items-center gap-x-12 xl:mb-12 xl:flex-row">
           <div className="pt-6">
             <h1 className="inline-block pb-6 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-              Hi, I'm Apurva Shah
+              Welcome to Cheatcodes
             </h1>
             <h2 className="prose text-lg text-gray-600 dark:text-gray-400">
               From developing projects for my community to one bagging around the world, I hope this
