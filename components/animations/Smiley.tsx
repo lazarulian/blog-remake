356d7a93e64a0ddc7b6c0b5e638477b6500b410d
Apurva Shah <shahh.apurva@gmail.com>
2024-10-01 01:14:20 -0700

    fixed smiley

--- components/animations/Smiley.tsx
+++ components/animations/Smiley.tsx
@@ -2,28 +2,26 @@ import React from 'react'
 
 const Smiley = () => {
   return (
-    <body>
-      <div className="rocketContainer">
-        <div className="moon">
-          <div className="crater crater1"></div>
-          <div className="crater crater2"></div>
-          <div className="crater crater3"></div>
-          <div className="crater crater4"></div>
-          <div className="crater crater5"></div>
-          <div className="shadow"></div>
-          <div className="eye eye-l"></div>
-          <div className="eye eye-r"></div>
-          <div className="mouth"></div>
-          <div className="blush blush1"></div>
-          <div className="blush blush2"></div>
-        </div>
-        <div className="orbit">
-          <div className="rocket">
-            <div className="window"></div>
-          </div>
+    <div className="rocketContainer">
+      <div className="moon">
+        <div className="crater crater1"></div>
+        <div className="crater crater2"></div>
+        <div className="crater crater3"></div>
+        <div className="crater crater4"></div>
+        <div className="crater crater5"></div>
+        <div className="shadow"></div>
+        <div className="eye eye-l"></div>
+        <div className="eye eye-r"></div>
+        <div className="mouth"></div>
+        <div className="blush blush1"></div>
+        <div className="blush blush2"></div>
+      </div>
+      <div className="orbit">
+        <div className="rocket">
+          <div className="window"></div>
         </div>
       </div>
-    </body>
+    </div>
   )
 }
 
